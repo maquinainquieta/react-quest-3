5b113562bbb1fa5f012259a3f815bbce0a2cc05f
maquinainquieta <asamimw@gmail.com>
2019-04-17 12:39:08 +0200

    It works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Quotes from "./Quotes";
+
+import Quote from "./Quote";
+
+const homer = {
+    quote:
+      "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+    character: "Homer Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+  }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+    working: true
+    };
+  }
   render() {
+    const x = this.state.working ? 'App-logo-on' : 'App-logo-off';
+    const func = () => {
+      this.setState({ working: !this.state.working});
+    };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={x} alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
-        <Quotes />
+        <button onClick={func} style={{fontSize:"200%"}}>Click Me</button>
+        <Quote {...homer}/>
       </div>
     );
   }
